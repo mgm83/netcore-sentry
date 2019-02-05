3b1bb33b1db516dc0011ceb213ace5172dfc6475
Mauro Morigi <mmorigi@lanacion.com.ar>
2019-02-05 00:37:59 -0300

    .

---
+++ docs/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+const RELEASE = '1.0.0'
+
+Sentry.init({ 
+    dsn: 'https://3db83a93652f45c8814854a9e8259efb@sentry.io/1386860',
+    release: RELEASE,
+    beforeSend(event) {
+        // Check if it is an exception, if so, show the report dialog
+        if (event.exception) {
+          Sentry.showReportDialog({
+              'title': 'Ups! Algo no ha salido bien...'
+          })
+        }
+        return event;
+    }
+});
